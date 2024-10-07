3135e45f7974253d53a26240311b3ba06ca8cb44
Cristian martini <cristianmartini.sp@gmail.com>
2024-10-07 11:26:40 -0300

    create react

---
+++ martini-site/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Cristian martini</h1>
+      <div className="card">
+       
+      </div>
+      <p className="read-the-docs">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis possimus atque aperiam inventore excepturi exercitationem eos nisi mollitia beatae maiores deserunt maxime expedita ducimus unde, temporibus, nobis et quas.
+      </p>
+    </>
+  )
+}
+
+export default App
